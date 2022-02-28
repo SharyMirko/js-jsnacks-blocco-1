81887bbcc79c5753e1543a17f642bc70091320ab
Mirko Moschella <shary.mirko@gmail.com>
2022-02-28 16:14:26 +0100

    snack del 28/02 completi

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 // SNACK 1.9
-
-/* let n = 0;
+let stamp_snack1 = document.querySelector('.snack1-9');
+let n = 0;
 let somma1 = 0;
 while (n <= 10) {
     somma1 += n;
     n++
 }
 let media1 = somma1 / 10;
-console.log(somma1)
-console.log(media1)
+
+stamp_snack1.innerHTML += `<br>` + 'fatto col ciclo while la somma è ' + somma1
+stamp_snack1.innerHTML += `<br>` + 'fatto col ciclo while la media è ' + media1
+
 
 
 let somma = 0;
@@ -17,19 +19,21 @@ for (i = 0; i <= 10; i++) {
     somma += i;
 }
 let media = somma / 10;
-console.log(somma)
-console.log(media) */
+
+stamp_snack1.innerHTML += `<br>` + 'fatto col ciclo for la somma è ' + somma1
+stamp_snack1.innerHTML += `<br>` + 'fatto col ciclo for la media è ' + media1
+
 
 
 // SNACK 2.1
-/* let stamp = document.querySelector('.snack2');
+let stamp = document.querySelector('.snack2-1');
 let snack2Somma = 0;
-let contatore = 0; */
+let contatore = 0;
 // ciclo for
-/* for (i = 0; i < 5; i++) {
+for (i = 0; i < 5; i++) {
     let numb = parseInt(prompt('dammi un numero'));
     snack2Somma += numb;
-} */
+}
 
 // ciclo while
 /* while (contatore < 5) {
@@ -37,22 +41,25 @@ let contatore = 0; */
     snack2Somma += numb;
     contatore++
 }
+ */
 
-
-stamp.innerHTML = snack2Somma */
-
+stamp.innerHTML += snack2Somma
 
 // SNACK 2.2
 let stamp_snack2_2 = document.querySelector('.snack2-2');
-let numberSnack2_2 = document.querySelector('#numero').value;
+let numberSnack2_2 = document.querySelector('#numero');
 let btnCheck = document.getElementById('check');
 
 btnCheck.addEventListener('click', function(){
-    if (numberSnack2_2 % 2 == 0) {
-        stamp_snack2_2.innerHTML = 'bravo! hai inserito un numero pari e cioè il numero' + numberSnack2_2
+    let numberSnack2_2Value = numberSnack2_2.value;
+    if (numberSnack2_2Value % 2 == 0) {
+        stamp_snack2_2.innerHTML = 'bravo! hai inserito un numero pari e cioè il numero ' + numberSnack2_2Value
+        numberSnack2_2.value = ''
     } else {
-        numberSnack2_2++
-        stamp_snack2_2.innerHTML = 'Hai inserito un numero dispari quindi lo aumento di 1 e diventa pari! Guarda: ' + numberSnack2_2
+        numberSnack2_2Value++
+        stamp_snack2_2.innerHTML = 'Hai inserito un numero dispari quindi lo aumento di 1 e diventa pari! Guarda: ' + numberSnack2_2Value
+        numberSnack2_2.value = ''
+
     }
 })
 
@@ -86,7 +93,7 @@ const arrCognomi = [
 ];
 let invitati = document.querySelector('.invitati');
 
-invitati.innerHTML = arrNomi[Math.floor(Math.random()*arrNomi.length)] + ' ' + arrCognomi[Math.floor(Math.random()*arrCognomi.length)] + `<br>` + arrNomi[Math.floor(Math.random()*arrNomi.length)] + ' ' + arrCognomi[Math.floor(Math.random()*arrCognomi.length)] + `<br>` + arrNomi[Math.floor(Math.random()*arrNomi.length)] + ' ' + arrCognomi[Math.floor(Math.random()*arrCognomi.length)] 
+invitati.innerHTML += arrNomi[Math.floor(Math.random()*arrNomi.length)] + ' ' + arrCognomi[Math.floor(Math.random()*arrCognomi.length)] + `<br>` + arrNomi[Math.floor(Math.random()*arrNomi.length)] + ' ' + arrCognomi[Math.floor(Math.random()*arrCognomi.length)] + `<br>` + arrNomi[Math.floor(Math.random()*arrNomi.length)] + ' ' + arrCognomi[Math.floor(Math.random()*arrCognomi.length)] 
 
 
 
